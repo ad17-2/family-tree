630cdd97d5c0fbc347d626977de020f7ed1bfec3
ari.finku <ari.stifandi@finku.id>
2024-12-16 08:13:07 +0700

    simplify get siblings

--- src/relationships/siblings.relationship.ts
+++ src/relationships/siblings.relationship.ts
@@ -3,12 +3,6 @@ import { RelationshipBase } from "./relationship.finder";
 
 export class SiblingsRelationship extends RelationshipBase {
   find(person: Person): string[] {
-    if (!person.mother) {
-      return [];
-    }
-    const siblings = person.mother.children
-      .filter((child) => child !== person)
-      .map((sibling) => sibling.name);
-    return siblings;
+    return this.getSiblings(person);
   }
 }
